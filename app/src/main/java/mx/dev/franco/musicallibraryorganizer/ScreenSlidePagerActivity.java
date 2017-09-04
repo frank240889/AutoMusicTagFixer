fbf0cced48854eedd1bc6300b902578d02bb01a1
Franco <francocastillo2@hotmail.com>
2017-09-04 17:33:21 -0500

    !Improved - Presentation of splash and first use tutorial, for making status bar translucent

--- app/src/main/java/mx/dev/franco/musicallibraryorganizer/ScreenSlidePagerActivity.java
+++ app/src/main/java/mx/dev/franco/musicallibraryorganizer/ScreenSlidePagerActivity.java
@@ -2,9 +2,8 @@ package mx.dev.franco.musicallibraryorganizer;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.MediaMetadataRetriever;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -14,11 +13,8 @@ import android.support.v4.view.ViewPager;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ListView;
-
-import java.io.File;
-import java.util.ArrayList;
 
 
 /**
@@ -39,7 +35,7 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
     private ViewPager mPager;
 
     /**
-     * The pager adapter, which provides the pages to the view pager widget.
+     * The pager adapter, which provides the pages to the swipeRefreshLayout pager widget.
      */
     private PagerAdapter mPagerAdapter;
     private static final int SWIPE_MIN_DISTANCE = 120;
@@ -54,6 +50,10 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_slide);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
         gestureDetector = new GestureDetector(this, new MyGestureDetector());
         gestureListener = new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
@@ -97,6 +97,9 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
                 mPager.setCurrentItem(mPager.getCurrentItem() + 1, true);
             }
         });
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabDots);
+        tabLayout.setupWithViewPager(mPager, true);
     }
 
     @Override
@@ -132,6 +135,21 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
             mPager.setCurrentItem(mPager.getCurrentItem() - 1);
         }
 
+
+
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        //View decorView = getWindow().getDecorView();
+// Hide both the navigation bar and the status bar.
+// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+// a general rule, you should design your app to hide the status bar whenever you
+// hide the navigation bar.
+        //int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        //decorView.setSystemUiVisibility(uiOptions);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, Win);
 
 
     }
