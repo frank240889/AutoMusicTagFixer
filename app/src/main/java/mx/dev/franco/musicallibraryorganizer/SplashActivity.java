07ecd10d70f6811ee4d0f5ab31c1ee53da82aed8
Franco <francocastillo2@hotmail.com>
2017-09-04 18:33:37 -0500

    !Added: - Internet connection jobScheduler. - Initial settings or retrieved the values saved. - Remove validation if DB doesn't exist.

--- app/src/main/java/mx/dev/franco/musicallibraryorganizer/SplashActivity.java
+++ app/src/main/java/mx/dev/franco/musicallibraryorganizer/SplashActivity.java
@@ -1,47 +1,78 @@
 package mx.dev.franco.musicallibraryorganizer;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.net.Uri;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.preference.PreferenceManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import java.io.File;
+import java.util.Set;
+
+import mx.dev.franco.musicallibraryorganizer.services.DetectorInternetConnection;
+import mx.dev.franco.musicallibraryorganizer.services.GnService;
+import mx.dev.franco.musicallibraryorganizer.services.Job;
 
 /**
  * Created by franco on 6/11/16.
  */
 
 public class SplashActivity extends AppCompatActivity{
-    //SharedPreferences lo utilizmos para guardar datos de la aplicacion, por ejemplo, para guardar la primer apertura, solo son datos accesibles por la app
-    protected static SharedPreferences sharedPreferences;
-    protected static SharedPreferences.Editor editor;
-    protected static boolean existDatabase = false;
+    //SharedPreferences we use for saving data from app, this data is only accessible for this app.
+    static SharedPreferences sharedPreferences;
+    static SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        //We check first if there is internet connection for initialize api
+        boolean isConnected = DetectorInternetConnection.isConnected(getApplicationContext());
+        if(isConnected) {
+            //We set context and initialize the GNSDK API.
+            GnService.setAppContext(this);
+            GnService.initializeAPI();
+
+        }
+        //No internet connection, then we schedule the job for initialize API
+        //when internet connection restores it
+        else {
+            Job.scheduleJob(getApplicationContext());
+        }
+
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        //We set the default values of settings in case the user had not modified them or first use
+        SelectedOptions.AUTOMATIC_CHANGE_FILENAME = preferences.getBoolean("title_service_change_switch", true);
+        SelectedOptions.MANUAL_CHANGE_FILE = preferences.getBoolean("title_manual_change_switch", false);
+        SelectedOptions.AUTOMATICALLY_REPLACE_STRANGE_CHARACTERS = preferences.getBoolean("title_automatically_replace_strange_chars",false);
+        SelectedOptions.ALL_SELECTED = preferences.getBoolean("allSelected",false);
+        SelectedOptions.SHOW_SEPARATORS = preferences.getBoolean("show_separators",false);
+
+        //Retrieve the string value saved in shared preferences first
+        String imageSizeSaved = preferences.getString("size_album_art","-1");
+        SelectedOptions.ALBUM_ART_SIZE = SelectedOptions.setValueImageSize(imageSizeSaved);
+
+        Set<String> stringSet = preferences.getStringSet("data_to_download",null);
+        SelectedOptions.setValuesExtraDataToDownload(stringSet);
+
+        //We set if is the first use of app
         sharedPreferences = getSharedPreferences("ShaPreferences", Context.MODE_PRIVATE);
         boolean firstTime= sharedPreferences.getBoolean("first", true);
 
+        RequiredPermissions.ACCESS_GRANTED_FILES = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+
         if(firstTime) {
-            Log.d("EXISTE DB","false");
+            //Is first app use
             Intent intent = new Intent(this, ScreenSlidePagerActivity.class);
             startActivity(intent);
         }
         else
         {
-            Log.d("EXISTE DB","true");
-            File db = getApplicationContext().getDatabasePath(DataTrackDbHelper.DATABASE_NAME);
-
-            if(db.exists()){
-                existDatabase = true;
-            }
-
             /*new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -57,5 +88,4 @@ public class SplashActivity extends AppCompatActivity{
         }
         finish();
     }
-
 }
