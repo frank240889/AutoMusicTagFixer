47774f1851892df6d509df610f7aad6b45ac315a
Franco <francocastillo2@hotmail.com>
2017-09-04 17:31:20 -0500

    !Added: - static field for reference permission access granted files.

--- app/src/main/java/mx/dev/franco/musicallibraryorganizer/RequiredPermissions.java
+++ app/src/main/java/mx/dev/franco/musicallibraryorganizer/RequiredPermissions.java
@@ -5,8 +5,9 @@ package mx.dev.franco.musicallibraryorganizer;
  */
 
 public final class RequiredPermissions {
-    protected static final int READ_INTERNAL_STORAGE_PERMISSION = 1;
-    protected static final int WRITE_INTERNAL_STORAGE_PERMISSION = 2;
-    protected static final int INTERNET_PERMISSION = 3;
-    protected static final int ACCESS_NETWORK_STATE = 4;
+    static final int READ_INTERNAL_STORAGE_PERMISSION = 1;
+    static final int WRITE_INTERNAL_STORAGE_PERMISSION = 2;
+    static final int INTERNET_PERMISSION = 3;
+    static final int ACCESS_NETWORK_STATE = 4;
+    static boolean ACCESS_GRANTED_FILES = false;
 }
