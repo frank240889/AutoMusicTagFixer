5f6acab196513d09b02ce6a61f3dbb67513a5fb9
Franco <francocastillo2@hotmail.com>
2017-09-04 19:42:16 -0500

    !Added: - New columns.
    !Removed:
    - TrackSelected table.

--- app/src/main/java/mx/dev/franco/musicallibraryorganizer/TrackContract.java
+++ app/src/main/java/mx/dev/franco/musicallibraryorganizer/TrackContract.java
@@ -11,6 +11,7 @@ public final class TrackContract {
 
     public static class TrackData implements BaseColumns{
         public static final String TABLE_NAME = "tracks_table";
+        static final String COLUMN_NAME_MEDIASTORE_ID = "mediastore_id";
         public static final String COLUMN_NAME_TITLE = "track_title";
         public static final String COLUMN_NAME_ARTIST = "track_artist";
         public static final String COLUMN_NAME_ALBUM = "track_album";
@@ -24,7 +25,7 @@ public final class TrackContract {
         public static final String COLUMN_NAME_GENRE = "track_genre";
         public static final String COLUMN_NAME_WRITER = "track_writer";
         public static final String COLUMN_NAME_FILE_TYPE = "track_file_type";
-        public static final String COLUMN_NAME_RESOLUTION = "track_resolution";
+        static final String COLUMN_NAME_RESOLUTION = "track_resolution";
         public static final String COLUMN_NAME_SAMPLING_RATE = "track_sampling_rate";
         public static final String COLUMN_NAME_BITRATE = "track_bitrate";
         public static final String COLUMN_NAME_CHANNELS = "track_channels";
@@ -35,14 +36,11 @@ public final class TrackContract {
         public static final String COLUMN_NAME_CURRENT_PATH = "track_current_path";
         public static final String COLUMN_NAME_CURRENT_FULL_PATH = "track_current_full_path";
         public static final String COLUMN_NAME_FILE_SIZE = "track_file_size";
+        public static final String COLUMN_NAME_IS_SELECTED = "is_selected";
+        static final String COLUMN_NAME_IS_VISIBLE = "is_visible";
+        static final String COLUMN_NAME_ADDED_RECENTLY = "added_recently";
         public static final String COLUMN_NAME_STATUS = "track_status"; //P for "processed", I for "incomplete data", NA for "no data avaliable", N for "no processed"
+        public static final String COLUMN_NAME_COVER_ART = "cover_art";
     }
 
-    public static class TrackSelected implements BaseColumns{
-        public static final String TABLE_NAME = "track_selected";
-        public static final String COLUMN_NAME_ID_TRACK = "id_track";
-        public static final String COLUMN_NAME_FILE_TYPE = "track_file_type";
-        public static final String COLUMN_NAME_CURRENT_FULL_PATH = "track_current_full_path";
-        public static final String COLUMN_NAME_STATUS = "track_status"; //P for "pending", C for "complete";
-    }
 }
